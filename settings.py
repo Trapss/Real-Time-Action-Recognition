6b7fa3c0226b94aea14fcd72fbe19da56f0e0684
Andrius <54733285+Recognize8607@users.noreply.github.com>
2023-09-29 16:03:32 +0300

    Update SORT max_age and min_hit

--- settings.py
+++ settings.py
@@ -9,6 +9,5 @@ winHeight = 480
 move_status = ['', 'stand', 'sit', 'walk', 'walk close', 'walk away', 'sit down', 'stand up']
 
 c = np.random.rand(32, 3) * 255
-sort_max_age = 20
-sort_min_hit = 3
-
+sort_max_age = 100
+sort_min_hit = 1
